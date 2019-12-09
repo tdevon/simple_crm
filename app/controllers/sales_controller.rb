d8dfe7425dc9f8f1a35e66096ab9d6798893035d
Tyler <tylerdevon01@gmail.com>
2019-12-09 11:47:20 -0500

    Refactor into partials

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,16 +1,7 @@
 class SalesController < ApplicationController
     def index
-        @sales = current_user.sales.all
-        @sales = @sales.sort_by(&:date).reverse!
-
-        if params[:sort].present?
-            if params[:sort] == "recent"
-                @sales = @sales.sort_by(&:date).reverse!
-            end
-            if params[:sort] == "amount"
-                @sales = @sales.sort_by(&:price).reverse!
-            end
-        end
+        @sales = current_user.sales.all.sort_by(&:date).reverse!
+        sort_sales
     end
     def show
         @sale = current_user.sales.find_by(id: params[:id])
@@ -44,9 +35,18 @@ class SalesController < ApplicationController
         @sale = current_user.sales.find_by(id: params[:id])
         @contacts = current_user.contacts.all
     end
-
 private
     def sale_params
         params.require(:sale).permit(:price, :date, :title, :description, :contact_id)
+    end
+    def sort_sales
+        if params[:sort].present?
+            if params[:sort] == "recent"
+                @sales = @sales.sort_by(&:date).reverse!
+            end
+            if params[:sort] == "amount"
+                @sales = @sales.sort_by(&:price).reverse!
+            end
+        end
     end
 end
